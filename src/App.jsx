1ae04609b04a9fbb6788fb5f904ceb5914a6ecc5
itaji-create <itaji.1999@gmail.com>
2022-03-17 16:27:25 -0300

    cria card que contem infos dos itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Foods from './pages/Foods';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
+      <Route path="foods" element={ <Foods /> } />
     </Routes>
   );
 }
---
+++ src/pages/Foods.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Foods() {
+  return (
+    <div>
+        ola foods
+    </div>
+    );
+  }
+  
+  export default Foods;
