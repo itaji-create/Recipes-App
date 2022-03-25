4290e84305e8fd9b058335a22e35e873261ecf9c
itaji-create <itaji.1999@gmail.com>
2022-03-25 13:38:58 -0300

    adiciona pagina de detalhes de bebida

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import Provider from './context/Provider';
 import Drinks from './pages/Drinks';
 import Profile from './pages/Profile';
 import RecipeMealDetails from './pages/RecipeMealDetails';
+import RecipeDrinkDetails from './pages/RecipeDrinkDetails';
 
 function App() {
-  const numbers = useLocation().pathname
-    .split('').filter((e) => (Number(e) || e === '0')).join('');
+  const { pathname } = useLocation();
+  let idNumbers = pathname
+  .split('').filter((e) => (Number(e) || e === '0')).join('');
+  if (!idNumbers) idNumbers = 52977;
   return (
     <Provider>
       <Routes>
-        <Route path="/" element={ <Login /> } />
-        <Route path="foods" element={ <Foods /> } />
-        <Route path="drinks" element={ <Drinks /> } />
-        <Route path="profile" element={ <Profile /> } />
-        <Route path={ `/foods/${numbers}` } element={ <RecipeMealDetails /> } />
+        <Route exact path="/" element={ <Login /> } />
+        <Route exact path="/foods" element={ <Foods /> } />
+        <Route exact path="/drinks" element={ <Drinks /> } />
+        <Route exact path="/profile" element={ <Profile /> } />
+        <Route exact path={ `foods/${idNumbers}` } element={ <RecipeMealDetails /> } />
+        <Route exact path={ `drinks/${idNumbers}` } element={ <RecipeDrinkDetails /> } />
       </Routes>
     </Provider>
   );
