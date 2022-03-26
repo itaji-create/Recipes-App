b3cc1f49177b0422c7d86c2cf55aa5075c3850bc
itaji-create <itaji.1999@gmail.com>
2022-03-25 21:48:46 -0300

    adiciona icones aos links explorer

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -7,21 +7,21 @@ function Explore() {
   return (
     <div className="allPage">
       <Header pageName="Explore" />
-      <div className="explorer-buttons">
+      <div>
         <Link to="/explore/foods">
           <button
             type="button"
-            className="profile-item"
+            className="explorer-item"
           >
-            Explore Foods
+            <img width={ '147px' } alt="Food Explorer" src={require('../images/explorer-food.jpg')} />
           </button>
         </Link>
         <Link to="/explore/drinks">
           <button
             type="button"
-            className="profile-item"
+            className="explorer-item"
           >
-            Explore Drinks
+            <img width={ '140px' } alt="Drink Explorer" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdGHRKOvyNolPLTsyVV2lbYdITSPnMI8ff3Q&usqp=CAU" />
           </button>
         </Link>
       </div>
