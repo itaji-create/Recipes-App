5d25f7b4ed7db872f2f6de8791ae91b1903055d8
Itaji de Carvalho <itaji.1999@gmail.com>
2023-09-26 20:20:44 -0300

    feat: tira a centralização de texto e aumenta tamanho do video

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,15 +1,18 @@
 function Instructions(props) {
   const { videoId, details } = props;
   return (
-    <div className="alert alert-success mb-5">
-      <p>Instructions</p>
-      <p  className='text-center' id="instructions" data-testid="instructions">{ details.strInstructions }</p>
+    <div gluid className="alert alert-success mb-5">
+      <b>Instructions:</b>
+      <p  className="" data-testid="instructions">{ details.strInstructions }</p>
+      <br />
       {videoId && (
         <div className='text-center'>
           <iframe
             title="recipe"
             src={ `https://www.youtube.com/embed/${videoId}` }
             frameBorder="0"
+            height={ '197' }
+            width={ '350' }
             allowFullScreen
           />
         </div>
