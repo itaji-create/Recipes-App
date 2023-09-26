a34eb35618fe66911afbd010b7e85e39fb7f78c6
Itaji de Carvalho <itaji.1999@gmail.com>
2023-09-26 20:08:21 -0300

    feat: usa todo espaço disponivel da tabela de ingredientes

--- src/components/IngredientsTable.js
+++ src/components/IngredientsTable.js
@@ -4,39 +4,37 @@ function IngredientsTable(props) {
   const { details } = props;
   return (
     <div className="container mt-5 mb-5">
-                <ul className="list-group">
-                  <li
-                    className="list-group-item"
-                  >
-                    <div className="row">
-                      <h5 className="col-sm-6">
-                        Ingredient
-                      </h5>
-                      <h5 className="col-sm-6">
-                        Quantity
-                      </h5>
-                    </div>
-                  </li>
-                  {ingredients(details).map((ingredient, i) => (
-                    details[ingredient] && (
-                        <li
-                          key={ ingredient }
-                          data-testid={ `${i}-ingredient-name-and-measure` }
-                          className="list-group-item"
-                        >
-                          <div className="row">
-                            <div className="col-sm-6">
-                              { details[ingredient] }
-                            </div>
-                            <div className="col-sm-6">
-                              { details[measures(details)[i]] }
-                            </div>
-                          </div>
-                        </li>
-                      )
-                  ))}
-                </ul>
-            </div>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <div className="row">
+            <h5 className="col">
+              Ingredient
+            </h5>
+            <h5 className="col">
+              Quantity
+            </h5>
+          </div>
+        </li>
+        {ingredients(details).map((ingredient, i) => (
+          details[ingredient] && (
+            <li
+              key={ ingredient }
+              data-testid={ `${i}-ingredient-name-and-measure` }
+              className="list-group-item"
+            >
+              <div className="row">
+                <div className="col">
+                  { details[ingredient] }
+                </div>
+                <div className="col">
+                  { details[measures(details)[i]] }
+                </div>
+              </div>
+            </li>
+            )
+        ))}
+      </ul>
+    </div>
   )    
 }
 
