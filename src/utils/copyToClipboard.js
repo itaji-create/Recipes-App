a12854addcab3c91969c9ada2ac096cd7df73933
Itaji de Carvalho <itaji.1999@gmail.com>
2023-09-27 20:12:17 -0300

    Update copyToClipboard.js

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -1,14 +1,15 @@
 const copy = require('clipboard-copy');
 
-function copyToClipboard(pathname) {
+function copyToClipboard() {
+  copy(window.location.href);
   const textField = document.createElement('p');
   textField.innerText = 'Link copied!';
   textField.id = 'copied';
   const buttons = document.getElementById('share-favorite');
-  if (!document.getElementById('copied')) {
-    buttons.appendChild(textField);
-  }
-  copy(`https://recipes-99app.herokuapp.com${pathname}`);
+  buttons.appendChild(textField);
+  setTimeout(() => {
+    textField.parentNode.removeChild(textField);
+  }, 2000);
 }
 
 export default copyToClipboard;
