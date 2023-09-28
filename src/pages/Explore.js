6d8592f9efa82aced6d1c9079b64f01eb3296113
itaji-create <itaji.1999@gmail.com>
2023-09-28 18:31:03 -0300

    fix: remove Link que não estava sendo utilizado

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Button from 'react-bootstrap/Button';
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import fetchFoods from "../services/fetchFoods";
 import Footer from "../components/Footer";
